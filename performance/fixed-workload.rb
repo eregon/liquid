595b941d35222e90bea0a7d4b28565e8f900c578
Benoit Daloze <eregontp@gmail.com>
2020-07-31 15:30:23 +0200

    Show max i/s

--- performance/fixed-workload.rb
+++ performance/fixed-workload.rb
@@ -2,6 +2,7 @@ ITERATIONS = Integer(ARGV[1] || 250_000)
 
 def bench
   t0 = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+  max = 0
   File.open('/dev/null', 'w') do |dev_null|
     File.open('/dev/urandom') do |dev_urandom|
       ITERATIONS.times do |i|
@@ -11,16 +12,20 @@ def bench
         dev_null.puts output
         elapsed = Process.clock_gettime(Process::CLOCK_MONOTONIC) - start
 
+        ips = (1.0/elapsed).floor
+        max = ips if ips > max
+
         run = i + 1
         if run < 10 or run % 10000 == 0
           puts run
-          puts (1.0/elapsed).floor
+          puts ips
         end
       end
     end
   end
   t1 = Process.clock_gettime(Process::CLOCK_MONOTONIC)
-  puts "TOTAL: #{'%.3f' % (t1-t0)}s"
+  total = t1 - t0
+  puts "TOTAL: #{'%.3f' % total} s, avg #{(ITERATIONS/total).to_i} i/s, max #{max} i/s"
 end
 
 require File.expand_path(ARGV.fetch(0))
